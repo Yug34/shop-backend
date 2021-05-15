c0879c20e75b0297153a9c6d509d502cdb317224
Yug34 <vertilax1337@gmail.com>
2021-05-15 19:30:53 +0530

    Removed index router

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
-let multer  = require('multer')
+let createError = require("http-errors");
+let express = require("express");
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let logger = require("morgan");
+let multer = require("multer");
 
-// let indexRouter = require('./routes/index');
-let catalogRouter = require('./routes/catalog');
+let catalogRouter = require("./routes/catalog");
 const Product = require("./models/product");
 
 let mongoose = require("mongoose");
 const cors = require("cors");
 const fs = require("fs");
-let mongoDB = "mongodb+srv://verti:yug123@cluster0.o2mor.mongodb.net/kabra-shop?retryWrites=true&w=majority";
+let mongoDB =
+  "mongodb+srv://verti:yug123@cluster0.o2mor.mongodb.net/kabra-shop?retryWrites=true&w=majority";
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 let db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
@@ -20,37 +20,36 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-// app.use('/', indexRouter);
-app.use('/catalog', catalogRouter);
+app.use("/catalog", catalogRouter);
 
-app.get('/', (req, res) => {
-  res.redirect('/catalog/list');
+app.get("/", (req, res) => {
+  res.redirect("/catalog/list");
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 // app.use(
